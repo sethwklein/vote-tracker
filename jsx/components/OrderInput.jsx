b8fddeae44f0804f4ebe82cc0ba3a4470ae7425b
Seth W. Klein <sk@sethwklein.net>
2017-10-02 12:34:04 -0400

    Try to render a PDF page
    I was seeing requests for files that we’re not serving. They’re
    in the /build directory, though. So we want the bundle to be
    asking for /static/foo.js when it’s actually asking for /foo.js.
    I’m not sure whether that’s a problem with the bundle, the
    server, or something else. — SK

--- jsx/components/OrderInput.jsx
+++ jsx/components/OrderInput.jsx
@@ -4,27 +4,61 @@ import PropTypes from 'prop-types';
 import OrderInputFields from '../components/OrderInputFields.jsx';
 
 const PDFJS = require('pdfjs-dist');
+
+var renderPDF = function() {
+  var start = function() {
+    console.log("starting");
+    getPDF();
+  };
+  var getPDF = function() {
+    console.log("getting document");
+    var url = 'http://cdn.mozilla.net/pdfjs/helloworld.pdf';
+    PDFJS.getDocument(url)
+      .then(pdf => getPage(null, pdf))
+      .catch(getPage);
+  };
+  var getPage = function(err, pdf) {
+    if (err) {
+      return console.log("Error: "+err);
+    }
+    console.log("got pdf");
+    pdf.getPage(1)
+      .then(page => render(err, page))
+      .catch(render);
+  };
+  var render = function(err, page) {
+    if (err) {
+      return console.log("Error:"+err);
+    }
+    console.log("got page");
+    // from example
+    var scale = 1.5;
+    var viewport = page.getViewport(scale);
+    var canvas = document.getElementById('pdf-canvas');
+    var context = canvas.getContext('2d');
+    canvas.height = viewport.height;
+    canvas.width = viewport.width;
+    var renderContext = {
+      canvasContext: context,
+      viewport: viewport,
+    };
+    // end example
+    console.log("rendering");
+    page.render(renderContext);
+  }
+
+  start();
+}
+
 // import * as fs from 'fs';
 // const fs = require('fs');
 
-// loadingTask.promise.then(function(pdf) {
-//   console.log('PDF loaded');
-// }
-
-
 const OrderInput = props => {
-
-  console.log("Hello from OrderInput");
-  var url = '//cdn.mozilla.net/pdfjs/helloworld.pdf';
-  console.log("We have a PDF.");
-  var loadingTask = PDFJS.getDocument(url);
-
-
+  renderPDF(); // hack! hack!
   return (
     <div>
-      <div className="pdfViewerWindow">PDF Goes here</div>
-
       <OrderInputFields />
+      <canvas style={{border: "1px dotted black"}} id="pdf-canvas"></canvas>
     </div>
   );
 }
