cb7c76ee91c991135b46aafb9f70cdb42e04bf06
Tyler Constance <tyler@tylerconstance.com>
2017-11-21 20:06:04 -0500

    Added a link to the input page

--- jsx/components/Overview.jsx
+++ jsx/components/Overview.jsx
@@ -9,6 +9,7 @@ const Overview = (props) => {
         <div className="section__body">
           <p>From the <a href="http://www.portlandmaine.gov/132/City-Council">city website</a>, &quot;The City of Portland operates under a Council-Manager form of government with a nine-member City Council, comprised of a popularly elected mayor, five district councilors and three at-large members. The mayor is elected by a majority of voters through ranked choice voting and serves a four-year term. The city’s five voting districts each elect one council member to represent their district and three at-large members are elected citywide. The eight councilors serve three-year staggered terms.</p>
           <p>The City Charter grants to the City Council all powers to enact, amend, or repeal rules, ordinances, and resolutions relating to the city’s property, affairs and government, to preserve the public peace, health and safety, to establish personnel policies, and giving effect to any vote of the city and to authorize the issuance of debt. The council adopts an annual budget for both municipal and school operations and provides for an annual audit. The council appoints the city manager, corporation counsel, and city clerk.&quot;</p>
+          <p>Go <Link to={`/input`}> input</Link> an order.</p>
         </div>
       </div>
     </section>
