550d6889457219da0a156e92574a8dcba794f864
Tyler Constance <tyler@tylerconstance.com>
2018-10-09 21:09:11 -0400

    Improved NoMatch component styles

--- jsx/components/NoMatch.jsx
+++ jsx/components/NoMatch.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 
 const NoMatch = () => {
   return (
-    <p>You've hit our 404 page. <a href="/">Go home</a>.</p>
+    <section className="section">
+      <div className="container">
+        <h1>That Page Doesn't Exist</h1>
+        <div className="section__body">
+            <p>You've hit our 404 page. <a href="/">Go home</a>.</p>
+        </div>
+      </div>
+    </section>
   );
 };
 
